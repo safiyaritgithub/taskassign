b28a65ef7c8f58fa3cd0107a64f74b035e3ce777
Sai Manikanta Bandaru <sai@rejola.com>
2023-08-28 20:07:04 +0530

    Improved signup ui

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        {/* <Navbar/> */}
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/signup" element={<Signupform/>}/>
